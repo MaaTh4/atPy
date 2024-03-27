426ae87b379a975d3b92cdf78d0468b4c4d746f8
MaaTh4 <127776316+MaaTh4@users.noreply.github.com>
2024-03-27 10:17:56 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -10,4 +10,9 @@ minha_string = input("digita algo: ")
 nova = re.sub(",", "", minha_string)
 novissima =  nova.replace(".", "")
 novapracaralho = novissima.replace(" ", "")
-print(novapracaralho)
+bolsonaro = unidecode(novapracaralho)
+print(bolsonaro)
+
+
+# string_invertida = inverter_string(minha_string)
+# print(string_invertida)
