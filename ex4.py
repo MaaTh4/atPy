3e7e999a78710e4a772f9657225b45d7baf98fd9
MaaTh4 <127776316+MaaTh4@users.noreply.github.com>
2024-03-28 09:35:14 -0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,14 +1,31 @@
 import random
 
 def rollDice(qntDados, numFaces):
-    primeiroDado = random.randint(1,6)
-    segundoDado = random.randint(1,6)
+    return sum(random.randint(1, numFaces) for x in range(qntDados))
+  
+def jogarCraps():  
+    qntDados = 2
+    numFaces = 6
+    
+    results = rollDice(qntDados, numFaces)   
+    
 
-sumDados = primeiroDado + segundoDado
-
-if sumDados == 7 or sumDados == 11:
-    print('Natural!!! Você ganhou!')
-elif sumDados == 2 or sumDados == 3 or sumDados == 12:
-    print('Craps!!! Você perdeu!')
-else:
-    print('Ponto')
+    if results == 7 or results == 11:
+        print(f'Natural!!! Você ganhou! Tirou: {results}')
+    elif results == 2 or results == 3 or results == 12:
+        print(f'Craps!!! Você perdeu! Tirou: {results}')
+    else:
+        print('Ponto', results)
+        ponto = results
+        while True:
+            segundaJogada = rollDice(qntDados,numFaces)
+            print(f'Sua segunda jogada foi: {segundaJogada}')
+            if segundaJogada == ponto:
+                print('SUA SEGUNDA JOGADA FOI PONTO!!! Você ganhou!')
+                break
+            elif segundaJogada == 7:
+                print('Você tirou 7! Você perdeu!')
+                break
+        
+        
+jogarCraps()
